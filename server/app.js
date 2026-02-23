c4a9d602db0d0bc10bc9111a69341d42e3f5c9b5
Alex-CA-04 <25bcnb04@kristujayanti.com>
2026-02-23 16:47:06 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,7 +13,8 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'admin',
-    database: process.env.DB_NAME || 'portfolio_db'
+    database: process.env.DB_NAME || 'portfolio_db',
+    port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
